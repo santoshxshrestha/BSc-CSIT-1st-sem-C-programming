f917c1c93dcac2e6f0e9d894b2238e2ee39add32
Santosh <santoshsantoshsthastha@gmail.com>
2024-12-01 19:13:14 +0545

    added new project

---
+++ area_and_vol_of_the_sphere.c
@@ -0,0 +1,16 @@
+/*Write a C program to find the area and volume of sphere and display the
+results. Formulas are:-
+Area = 4*PI*R*R Volume = 4/3*PI*R*R*R.
+Note : Assume PI=3.14 and take the value of R from user*/
+#include<stdio.h>
+#define PI 3.14
+int main() {
+    float R,Area,Volume;
+    printf("Enter the radius of the sphere\n=");
+    scanf("%f",&R);
+    Area =  4*PI*R*R;
+    printf("The area fo the sphere is %.2f\n",Area);
+    Volume = 4/3*PI*R*R*R;
+    printf("The volume of the sphere is %.2f\n",Volume);
+    return 0;
+}
