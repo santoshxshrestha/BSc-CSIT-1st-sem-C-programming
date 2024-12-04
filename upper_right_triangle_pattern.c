ac103aa83b772f8e5be9cfa711d57ecf327b03f5
Santosh <santoshsantoshsthastha@gmail.com>
2024-12-04 16:40:11 +0545

    fixed minor error

--- upper_right_triangle_pattern.c
+++ upper_right_triangle_pattern.c
@@ -2,7 +2,7 @@
 int main() {
     int i,j;
     char character;
-    printf("Enter the character which you want to use to make the pattern=");
+    printf("Enter the character which you want to use to make the pattern:");
     scanf("%c",&character);
     for(i=0; i<=5; i++){
         for(j=0; j<i; j++){
