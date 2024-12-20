c0c735a838b533b1d7e59a53185bfd0e96ce3fce
Santosh <santoshsantoshsthastha@gmail.com>
2024-12-20 17:37:42 +0545

    one step more with some tests file

---
+++ rough.c
@@ -0,0 +1,28 @@
+/*#include <stdio.h>
+// int main() {
+//     int x;
+//     x = 20;
+//     x=12;
+//     printf("\nx=%d",x);
+//     return 0;
+// // }*/
+// // #include<stdio.h>
+// // int main () {
+// //     int num[4];
+// //     for(int i=0;i<4;i++)
+// //     {
+// //         printf("enter the number=");
+// //         scanf("%d",&num[i]);
+// //     }
+// //       for(int i=0;i<4;i++)
+// //     {
+// //         printf("the numbers are %d\n",num[i]);
+// //     }
+// //     return 0;
+// // }
+#include<stdio.h>
+int main() {
+    santosh();
+    printf("my name is santosh");
+}
+santosh()
