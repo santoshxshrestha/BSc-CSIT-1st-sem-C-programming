9034b75baf71fa217e6e694fd3b65f8ae5fd8f14
Santosh <santoshsantoshsthastha@gmail.com>
2024-12-25 15:50:02 +0545

    did some modification with the palindrome_test.c

--- palindrome_test.c
+++ palindrome_test.c
@@ -1,4 +1,22 @@
+#include<string.h>
 #include <stdio.h>
 int main() {
-    
+    char str[100];
+    char rev_str[100];
+    int len = strlen(str);
+    printf("Enter a string = ");
+    fgets(str, sizeof(str), stdin); // fgets is safe then gets (learn about buffer size for gets)
+    str[strcspn(str, "\n")] = '\0';
+
+    strcpy(rev_str, str);
+    strrev(rev_str);
+
+    for(int i=0, i<len, i++){
+        str[i] = rev_str[i];
+        printf("The inputed string is palindrome");
+    }
+    else{
+        printf("The inputed string is not palindrome");
+    }
+    return 0;
 }
