de0f1641d51c49495bfcd44345029593192a98f3
Santosh <santoshsantoshsthastha@gmail.com>
2025-01-03 09:51:24 +0545

    added the line that will just help with the length of the array

--- greatest_in_array.c
+++ greatest_in_array.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
-int large(int[]);
+int large(int[], int);
 int main() {
     int nums[] = {20,40,30,45,15};
-    large(nums);
+    int length = sizeof(nums)/sizeof(nums[0]);
+    large(nums,length);
     return 0;
 }
 
-int large(int nums[]) {
+int large(int nums[], int length) {
     int greatest = nums[0];
-    for(int i = 1; i<5; i++) {
+    for(int i = 1; i<length; i++) {
         if (greatest<nums[i]) {
             greatest = nums[i];
         }
