2a09decfdfec23138128be3e43d75641a2db7618
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-02-13 21:00:33 +0545

    some use of the proper things

--- pointer.c
+++ pointer.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main() {
     int age = 20, *page;
-    float salary = 40000, *psalary;
+    float salary = 40000.5334, *psalary;
     page = &age;
     psalary = &salary;
-    printf("Address of age: %u and value of age: %d \n", page, *page);
-    printf("Address of salary: %u and value of address is: %f \n", psalary, *psalary);
+    printf("Address of age: %p and value of age: %d \n", page, *page);
+    printf("Address of salary: %p and value of address is: %.2f \n", psalary, *psalary);
 }
