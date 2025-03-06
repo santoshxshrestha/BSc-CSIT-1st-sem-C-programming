185f54bd84feea2ef65e2bdc1879e583bc61bbd1
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-03-06 19:41:13 +0545

    some changes

--- temp.c
+++ temp.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int a=3;
 int b;
+//here it is the similar thing to the function prototype but it is not the one 
+//it is call the ceclaration of the global variable 
 
 int main () {
     int sum = a+b;
