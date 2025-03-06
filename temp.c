7a82be1578e4da76cf62f9c3f9d3b39d97f1c70e
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-03-06 19:36:54 +0545

    git ignore

---
+++ temp.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int a=3;
+int b;
+
+int main () {
+    int sum = a+b;
+    printf("The sum of the number: %d with another number: %d is %d.\n",a, b, sum);
+}
+
+int b = 20;
+int c = 32;
