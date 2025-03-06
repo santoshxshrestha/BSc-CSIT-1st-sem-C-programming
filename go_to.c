c7d5b9966c5f0d60a0fe3d756b0bed26a5134948
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-03-06 20:34:22 +0545

    the go to

---
+++ go_to.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main () {
+    int a = 0;
+again:
+    printf("Enter a positive number: ");
+    scanf("%d",&a);
+
+    if(a<0){
+        printf("Enter a positive number you have entered: %d\n", a);
+        goto again;
+    }else {
+    printf("you have entered the positive number and the number is: %d\n",a);
+    }
+
+
+    
+}
