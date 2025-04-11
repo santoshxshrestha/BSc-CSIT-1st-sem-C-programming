36db15b77f63a01f7f935ac0fa2a3e6cfda7e600
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-04-11 23:32:57 +0545

    lol

--- rough.c
+++ rough.c
@@ -1,29 +1,8 @@
-/*#include <stdio.h>
-// int main() {
-//     int x;
-//     x = 20;
-//     x=12;
-//     printf("\nx=%d",x);
-//     return 0;
-// // }*/
-// // #include<stdio.h>
-// // int main () {
-// //     int num[4];
-// //     for(int i=0;i<4;i++)
-// //     {
-// //         printf("enter the number=");
-// //         scanf("%d",&num[i]);
-// //     }
-// //       for(int i=0;i<4;i++)
-// //     {
-// //         printf("the numbers are %d\n",num[i]);
-// //     }
-// //     return 0;
-// // }
 #include <stdio.h>
-int main() {
-    int a, b ;
-    printf("enter the two numbers ");
-    scanf("%d %d", &a, &b);
-    printf("the value stored are %d , %d",a,b);
+
+int main(void) {
+   int a = 90909;
+   int *p ;
+   p = &a;
+   printf("The pointer you are looking for is : %p",p);
 }
