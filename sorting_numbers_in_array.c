a21803e08cbacb087a92fe00bcfc04fc6c0ac52f
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-04-12 14:32:51 +0545

    some changes in the thing

--- sorting_numbers_in_array.c
+++ sorting_numbers_in_array.c
@@ -20,7 +20,7 @@ void print(int arr[], int n){
 
 void order(int arr[], int n) {
     for(int i = 0; i<n; i++) {
-        for(int j = 0; j<n-i-1; j++){
+        for(int j = 0; j<n-i; j++){
             if(arr[j]>arr[j+1]){
                 int temp = arr[j+1];
                 arr[j+1] = arr[j];
