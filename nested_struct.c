dd708c4c3bbe1cc157f14fd2d7c2c0c132842bb2
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-04-12 17:04:29 +0545

    Update nested_struct.c
    added some char type in the nested struct

--- nested_struct.c
+++ nested_struct.c
@@ -6,6 +6,7 @@ struct student {
         int year;
         int month;
         int day;
+        char week_days[20];
     }d;
 }s;
 int main() {
@@ -13,15 +14,17 @@ int main() {
     scanf("%s",s.name);
     printf("Enter the class of the student :");
     scanf("%d",&s.class);
-    printf("Enter the year of birth");
+    printf("Enter the year of birth :");
     scanf("%d",&s.d.year);
-    printf("Enter the month of birth");
+    printf("Enter the month of birth :");
     scanf("%d",&s.d.month);
-    printf("Enter the day of birth");
+    printf("Enter the day of birth :");
     scanf("%d",&s.d.day);
+    printf("Enter hte week days of the birth :");
+    scanf("%s",s.d.week_days);
 
-    printf("The name of the student is %s",s.name);
-    printf("The class of the student is %d",s.class);
-    printf("The date of birth of the student is %d-%d-%d",s.d.year,s.d.month,s.d.day);
+    printf("The name of the student is %s.\n",s.name);
+    printf("The class of the student is %d.\n",s.class);
+    printf("The date of birth of the student is %d-%d-%d %s.\n",s.d.year,s.d.month,s.d.day,s.d.week_days);
 return 0;
 }
