c5d39b49b5c32952a0bc26ae6a6192ef55ff37a7
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-04-13 11:02:37 +0545

    Update globalvar.c
    typo

--- globalvar.c
+++ globalvar.c
@@ -3,7 +3,7 @@ int a=3;
 int b;
 int c;
 //here it is the similar thing to the function prototype but it is not the one 
-//it is call the ceclaration of the global variable 
+//it is call the declaration of the global variable 
 
 int main () {
     int sum = a+b;
