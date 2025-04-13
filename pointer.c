d2d71e3c0611a1df50e48543ca1130be8345fa7a
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-04-13 11:24:53 +0545

    the sign

--- pointer.c
+++ pointer.c
@@ -4,6 +4,6 @@ int main() {
     float salary = 40000.5334, *psalary;
     page = &age;
     psalary = &salary;
-    printf("Address of age: %p and value of age: %d \n", page, *page);
-    printf("Address of salary: %p and value of address is: %.2f \n", psalary, *psalary);
+    printf("Address of age: %p and value of age: %d \n", &page, *page);
+    printf("Address of salary: %p and value of address is: %.2f \n", &psalary, *psalary);
 }
